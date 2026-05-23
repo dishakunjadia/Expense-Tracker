58e15116703984aa395133e08129a6f91c0a1fed
dishakunjadia <kunjadiadisha@gmail.com>
2026-05-23 10:06:02 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Disha Kunjadia on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
